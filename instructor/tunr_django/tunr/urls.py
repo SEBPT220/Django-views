5998bb9d1678cf870c8e3052a1b8391862c2fe1b
Denis S Dujota <denisdujota@gmail.com>
2023-12-20 20:53:11 -0500

    update up to show route

---
+++ instructor/tunr_django/tunr/urls.py
@@ -0,0 +1,11 @@
+
+# This is the same as a router file in Express
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path('', views.artist_list, name='artist_list'),
+  path('songs/', views.song_list, name='song_list'),
+  path('artists/<int:pk>', views.artist_detail, name='artist_detail')
+]
--- instructor/tunr_django/tunr/views.py
+++ instructor/tunr_django/tunr/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from .models import Artist, Song
 # Create your views here.
+
+# Role of controller
+# - process http request
+# - gather the necessary resources (information from the system <Models>)
+# - pass the resources to the template
+
+def artist_list(request):
+  artists = Artist.objects.all()
+  return render(request, 'artist_list.html', {'artists': artists})
+
+def song_list(request):
+  songs = Song.objects.all()
+  return render(request, 'song_list.html', {'songs': songs})
+
+def artist_detail(request, pk):
+  artist = Artist.objects.get(id=pk)
+  return render(request, 'artist_detail.html', {'artist': artist})

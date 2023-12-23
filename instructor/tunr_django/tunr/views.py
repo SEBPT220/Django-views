47cbc6441321f457e8fa8f2af83c3a0a45fa0560
Denis S Dujota <denisdujota@gmail.com>
2023-12-23 13:07:50 -0500

    finish crud songs and artist

--- instructor/tunr_django/tunr/views.py
+++ instructor/tunr_django/tunr/views.py
@@ -12,17 +12,9 @@ def artist_list(request):
   artists = Artist.objects.all()
   return render(request, 'artist_list.html', {'artists': artists})
 
-def song_list(request):
-  songs = Song.objects.all()
-  return render(request, 'song_list.html', {'songs': songs})
-
 def artist_detail(request, pk):
   artist = Artist.objects.get(id=pk)
   return render(request, 'artist_detail.html', {'artist': artist})
-
-def song_detail(request, pk):
-    song = Song.objects.get(id=pk)
-    return render(request, 'song_detail.html', {'song': song})
 
 def artist_create(request):
     if request.method == 'POST':
@@ -35,6 +27,36 @@ def artist_create(request):
 
     return render(request, 'artist_form.html', {'form': form})
 
+# EDIT ARTIST
+def artist_edit(request, pk):
+    artist =  Artist.objects.get(id=pk)
+
+    if request.method == 'POST':
+      form = ArtistForm(request.POST, instance=artist)
+      if form.is_valid():
+        artist = form.save()
+        return redirect('artist_detail', pk=artist.pk)
+    else:
+      form = ArtistForm(instance=artist)
+
+    return render(request, 'artist_form.html', {'form': form})
+
+# _ convention for unused variables required for positional arguments
+def artist_delete(_, pk):
+    artist =  Artist.objects.get(id=pk)
+    artist.delete()
+    return redirect('artist_list')
+
+# SONGS
+
+def song_list(request):
+  songs = Song.objects.all()
+  return render(request, 'song_list.html', {'songs': songs})
+
+def song_detail(request, pk):
+    song = Song.objects.get(id=pk)
+    return render(request, 'song_detail.html', {'song': song})
+
 def song_create(request):
     if request.method == 'POST':
       form = SongForm(request.POST)
@@ -45,3 +67,20 @@ def song_create(request):
       form = SongForm()
 
     return render(request, 'song_form.html', {'form': form})
+
+def song_edit(request, pk):
+    song = Song.objects.get(id=pk)
+    if request.method == 'POST':
+      form = SongForm(request.POST, instance=song)
+      if form.is_valid():
+        song = form.save()
+        return redirect('song_detail', pk=song.pk)
+    else:
+      form = SongForm(instance=song)
+
+    return render(request, 'song_form.html', {'form': form})
+
+def song_delete(_, pk):
+    song = Song.objects.get(id=pk)
+    song.delete()
+    return redirect('song_list')
